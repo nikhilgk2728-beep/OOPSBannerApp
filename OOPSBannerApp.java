25b24e1cfab703a7dbf40ed350c9cb8a6c57fff4
nikhilgk2728-beep <nikhilgk2728@gmail.com>
2026-03-03 19:41:48 +0530

    printing oops banner using string join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,20 @@
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        // This line prints the text to the console
-        System.out.println("OOPS");
+
+        String[] lines = new String[7];
+
+        // O      O      P      S
+        lines[0] = String.join(" ", " ***  ", " ***  ", " **** ", " **** ");
+        lines[1] = String.join(" ", "*   * ", "*   * ", "*   * ", "*     ");
+        lines[2] = String.join(" ", "*   * ", "*   * ", "*   * ", "*     ");
+        lines[3] = String.join(" ", "*   * ", "*   * ", " **** ", " **** ");
+        lines[4] = String.join(" ", "*   * ", "*   * ", "*     ", "     *");
+        lines[5] = String.join(" ", "*   * ", "*   * ", "*     ", "     *");
+        lines[6] = String.join(" ", " ***  ", " ***  ", "*     ", " **** ");
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
